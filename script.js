782baeaef79c6c5972ae5ece28353e2cb27b2cd4
Isaac🎲 <cassioitavom@gmail.com>
2024-09-17 10:47:20 -0300

    2° Commit. Reponsive? IDK, i hope so.

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 import { data } from './src/data.js';
 
-function createCard(title, banner, date, hour, tags, content) {
+function createCard(title, banner, date, tags, content) {
     let card = document.createElement("div");
     
     let contentId = `content-${Math.random().toString(36).substr(2, 9)}`;
 
     card.innerHTML = `
 <div class="card-body" style="cursor: pointer;">
-    <p class="card-date">${date} - ${hour}</p>
-    <img class="card-banner" src="${banner}"></img>
-    <h1 class="card-title">${title}</h1>
-    <div class="card-tags">${tags}</div>
-
+    <section id="card-header">
+        <p class="card-date"> <b>${date} </b> </p>
+        <img class="card-banner" src="${banner}"></img>
+        <div class="card-tags">${tags}</div>
+        <h1 class="card-title">${title}</h1>
+    </section>
     <div id="${contentId}" class="content" style="display: none;">
         <div class="card-text"> ${content} </div>
-
+        <br><br>
+        <a class="up-btn" href="#card-header"> <img src="https://cdn3.iconfinder.com/data/icons/faticons/32/arrow-up-01-512.png"></a>
     </div>
 </div>
 `;
 
-    card.querySelector(".card-body").addEventListener("click", function() {
+    card.querySelector("#card-header").addEventListener("click", function() {
         let contentElement = card.querySelector(`#${contentId}`);
         
         if (contentElement.style.display === "none") {
@@ -36,6 +38,6 @@ let contentContainer = document.querySelector(".content");
 for (let key in data) {
     if (data.hasOwnProperty(key)) {
         let item = data[key];
-        contentContainer.appendChild(createCard(item.title, item.banner, item.date, item.hour, item.tags, item.content));
+        contentContainer.appendChild(createCard(item.title, item.banner, item.date, item.tags, item.content));
     }
 }
