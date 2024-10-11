73e04fb08e70541aecd17ebf73d43fa2e280665f
Isaac🎲 <cassioitavom@gmail.com>
2024-10-11 18:32:30 -0300

    Commit 4.3 - Definily this must be work.
    I think this could work to resolve the infernal bug on the loading of the tmeplate.html

--- load.js
+++ load.js
@@ -5,7 +5,7 @@ export function createCard(metod, type, title, tags, desc, id, content = '') {
 
     if (metod === "load") {
         card.innerHTML = `
-        <a href='./postagem.html?id=${id}' target="_blank">
+        <a href='./template.html?id=${id}' target="_blank">
             <div class="card-preview" id="${type}" style="cursor: pointer;">
                 <div class="card-tags">${tags}</div>
                 <h1 class="card-title">${title}</h1>
