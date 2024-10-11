1e9e59f4ddd36c833644930a973f1b09d69ed212
Isaac🎲 <cassioitavom@gmail.com>
2024-10-11 18:26:07 -0300

    Update load.js
    Testando para ver se o culpado é o arquivo de load.js

--- load.js
+++ load.js
@@ -1,14 +1,12 @@
 import { data } from './src/data.js';
 
-// Função que cria cards com base no método
-export function createCard(metod, title, tags, desc, id, content = '') {
+export function createCard(metod, type, title, tags, desc, id, content = '') {
     let card = document.createElement("div");
 
-    // Card de pré-visualização ("load")
     if (metod === "load") {
         card.innerHTML = `
-        <a href='./template.html?id=${id}' target="_blank">
-            <div class="card-preview" style="cursor: pointer;">
+        <a href='./postagem.html?id=${id}' target="_blank">
+            <div class="card-preview" id="${type}" style="cursor: pointer;">
                 <div class="card-tags">${tags}</div>
                 <h1 class="card-title">${title}</h1>
                 <p class="card-descr">${desc}</p>
@@ -18,10 +16,9 @@ export function createCard(metod, title, tags, desc, id, content = '') {
         return card;
     }
 
-    // Card de visualização completa ("view")
     else {
         card.innerHTML = `
-        <div class="article" style="cursor: pointer;">
+        <div class="article" id="${type}" style="cursor: pointer;">
             <h1 class="title">${title}</h1>
             <div class="tags">${tags}</div>
             <p class="content">${content}</p>
@@ -31,11 +28,10 @@ export function createCard(metod, title, tags, desc, id, content = '') {
     }
 }
 
-// Carregando os cards de pré-visualização para o index
 let contentContainer = document.querySelector(".content");
 for (let key in data) {
     if (data.hasOwnProperty(key)) {
         let item = data[key];
-        contentContainer.appendChild(createCard("load", item.title, item.tags, item.descricao, key));
+        contentContainer.appendChild(createCard("load",item.type, item.title, item.tags, item.descricao, key));
     }
 }
